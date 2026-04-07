fb10bc6ba73ed94f961e98e5981f9d3a95e856b1
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-07 05:22:37 +0200

    Configure Flask app and SQLite database URI

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
- 
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+import os
+
+# from data_models import db, Author, Book
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
