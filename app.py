e3d37ebe1ddb146ca69e52cea754446e21b05160
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-07 06:03:52 +0200

    Add Author and Book models and create database tables

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
-from data_models import db
+from data_models import db, Author, Book
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 
-# db.init_app(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
--- data_models.py
+++ data_models.py
@@ -1,3 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+    def __str__(self):
+        return self.name
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(20), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
+
+    author = db.relationship("Author", backref=db.backref("books", lazy=True))
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    def __str__(self):
+        return self.title
